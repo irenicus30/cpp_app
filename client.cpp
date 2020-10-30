ebdacde49301da4f5537e5acf7cc195d33e1d4d5
Mar Sob <irenicus30@gmail.com>
2020-10-30 17:17:24 +0100

    first working version of api

---
+++ client.cpp
@@ -0,0 +1,136 @@
+#include <grpcpp/grpcpp.h>
+#include <string>
+#include "myapi.grpc.pb.h"
+
+using grpc::Channel;
+using grpc::ClientContext;
+using grpc::Status;
+
+using myapi::UserData;
+using myapi::RegisterUserData;
+using myapi::Users;
+using myapi::RegisterReply;
+using myapi::Page;
+using myapi::PrimaryKey;
+using myapi::UserService;
+
+class UserServiceClient {
+ public:
+  UserServiceClient(std::shared_ptr<Channel> channel)
+      : stub_(UserService::NewStub(channel)) {}
+
+  // Assembles client payload, sends it to the server, and returns its response
+  void Register() {
+    // Data to be sent to server
+    RegisterUserData registerUserData;
+
+    registerUserData.set_email("dummy email");
+    registerUserData.set_nickname("dummy nickname");
+    registerUserData.set_is_active(true);
+    registerUserData.set_password("secret");
+
+    // Container for server response
+    RegisterReply reply;
+
+    // Context can be used to send meta data to server or modify RPC behaviour
+    ClientContext context;
+
+    // Actual Remote Procedure Call
+    Status status = stub_->Register(&context, registerUserData, &reply);
+
+    // Returns results based on RPC status
+    if (status.ok()) {
+      if (reply.status()) {
+        std::cout << "user registered" << std::endl;
+      } else {
+        std::cout << "user not registered" << std::endl;
+      }
+    } else {
+      std::cout << status.error_code() << ": " << status.error_message()
+                << std::endl;
+      std::cout << "RPC Failed" << std::endl;
+    }
+  }
+  
+  void EnumerateRecords(int page_number, int page_size) {
+    // Data to be sent to server
+    Page page;
+    page.set_page_number(page_number);
+    page.set_page_size(page_size);
+
+    // Container for server response
+    Users users;
+
+    // Context can be used to send meta data to server or modify RPC behaviour
+    ClientContext context;
+
+    // Actual Remote Procedure Call
+    Status status = stub_->EnumerateRecords(&context, page, &users);
+
+    // Returns results based on RPC status
+    if (status.ok()) {
+      std::cout << "EnumerateRecords ok" << std::endl;
+      std::cout << "users.entry_size() " << users.entry_size() << std::endl;
+      for(int i = 0; i < users.entry_size(); i++) {
+        UserData user = users.entry(i);
+        std::cout << user.email() << " " << user.nickname() << " " << user.is_active() << std::endl;
+      }
+    } else {
+      std::cout << status.error_code() << ": " << status.error_message()
+                << std::endl;
+      std::cout << "RPC Failed" << std::endl;
+    }
+  }
+  
+  void GetRecord(int index) {
+    // Data to be sent to server
+    PrimaryKey key;
+    key.set_index(index);
+
+    // Container for server response
+    UserData user;
+
+    // Context can be used to send meta data to server or modify RPC behaviour
+    ClientContext context;
+
+    // Actual Remote Procedure Call
+    Status status = stub_->GetRecord(&context, key, &user);
+
+    // Returns results based on RPC status
+    if (status.ok()) {
+      std::cout << "User:" << std::endl;
+      std::cout << user.email() << " " << user.nickname() << " " << user.is_active() << std::endl;
+    } else {
+      std::cout << status.error_code() << ": " << status.error_message()
+                << std::endl;
+      std::cout << "RPC Failed" << std::endl;
+    }
+  }
+
+ private:
+  std::unique_ptr<UserService::Stub> stub_;
+};
+
+void RunClient() {
+  std::string target_address("0.0.0.0:9090");
+  // Instantiates the client
+  UserServiceClient client(
+      // Channel from which RPCs are made - endpoint is the target_address
+      grpc::CreateChannel(target_address,
+                          // Indicate when channel is not authenticated
+                          grpc::InsecureChannelCredentials()));
+
+
+  client.Register();
+  client.EnumerateRecords(5, 5);
+  client.GetRecord(5);
+  client.EnumerateRecords(5, 5);
+
+  std::cout << "Done" << std::endl;
+}
+
+int main(int argc, char* argv[]) {
+  RunClient();
+
+  return 0;
+}
---
+++ server.cpp
@@ -0,0 +1,81 @@
+#include <grpcpp/grpcpp.h>
+#include <string>
+#include "myapi.grpc.pb.h"
+
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::Status;
+
+using myapi::UserData;
+using myapi::RegisterUserData;
+using myapi::Users;
+using myapi::RegisterReply;
+using myapi::Page;
+using myapi::PrimaryKey;
+using myapi::UserService;
+
+// Server Implementation
+class UserServiceImplementation final : public UserService::Service {
+  Status Register(ServerContext* context, const RegisterUserData* registerUserData,
+                  RegisterReply* reply) override {
+    
+    std::cout << "Register" << std::endl;
+
+    reply->set_status(true);
+    return Status::OK;
+  }
+  
+  Status EnumerateRecords(ServerContext* context, const Page* page,
+                          Users* users) override {
+    
+    std::cout << "EnumerateRecords" << std::endl;
+
+    UserData *u1 = users->add_entry();
+    u1->set_email("email from database list 1");
+    u1->set_nickname("nickname from database list 1");
+    u1->set_is_active(false);
+    
+    UserData *u2 = users->add_entry();
+    u2->set_email("email from database list 2");
+    u2->set_nickname("nickname from database list 2");
+    u2->set_is_active(true);
+
+    return Status::OK;
+  }
+  
+  Status GetRecord(ServerContext* context, const PrimaryKey* key,
+                   UserData* user) override {
+    
+    std::cout << "GetRecord" << std::endl;
+
+    user->set_email("email from database");
+    user->set_nickname("nickname from database");
+    user->set_is_active(true);
+
+    return Status::OK;
+  }
+};
+
+void RunServer() {
+  std::string server_address("0.0.0.0:9090");
+  UserServiceImplementation service;
+
+  ServerBuilder builder;
+  // Listen on the given address without any authentication mechanism
+  builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+  // Register "service" as the instance through which
+  // communication with client takes place
+  builder.RegisterService(&service);
+
+  // Assembling the server
+  std::unique_ptr<Server> server(builder.BuildAndStart());
+  std::cout << "Server listening on port: " << server_address << std::endl;
+
+  server->Wait();
+}
+
+int main(int argc, char** argv) {
+  RunServer();
+  return 0;
+}
